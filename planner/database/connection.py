de621b3f461f8b0b24dc62ff122f3fbec46fc25e
Seunghun Cho <anyjo1015@gmail.com>
2023-09-18 00:01:43 +0900

    beanie try

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -1,14 +1,14 @@
-from sqlmodel import SQLModel, Session, create_engine
-from models.events import Event
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from pydantic import BaseSettings
 
-database_file = "planner.db"
-database_connection_string = f"sqlite:///{database_file}"
-connect_args = {"check_same_thread": False}
-engine_url = create_engine(database_connection_string, echo=True, connect_args=connect_args)
+class Settings(BaseSettings):
+    DATABASE_URL: Optional[str] = None
 
-def conn():
-    SQLModel.metadata.create_all(engine_url)
-    
-def get_session():
-    with Session(engine_url) as session:
-        yield session
+    async def initialized_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(database=client.get_default_database, document_models=[])
+
+    class Config:
+        env_file = ".env"
